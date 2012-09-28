ed4b549ab83fe2942a6d4923753f1349e6d6dde3
Kasper Souren <kasper@guaka.org>
2012-09-28 12:03:49 +0200

    more

--- leaderboard.js
+++ leaderboard.js
@@ -4,41 +4,50 @@
 Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
-  Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
-  };
-
-  Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-  };
-
-  Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
-
-  Template.leaderboard.events({
-    'click input.inc': function () {
-      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
-
-  Template.player.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  });
-
-
-    var somewhat_uuid = function() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-            return v.toString(16);
-        });
-    }
+    Template.leaderboard.players = function () {
+        return Players.find({}, {sort: {score: -1, name: 1}});
+    };
     
+    Template.leaderboard.selected_name = function () {
+        var player = Players.findOne(Session.get("selected_player"));
+        return player && player.name;
+    };
+    
+    Template.player.selected = function () {
+        return Session.equals("selected_player", this._id) ? "selected" : '';
+    };
+    
+    Template.leaderboard.events({
+        'click input.inc': function () {
+            Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+        }
+    });
+    
+    Template.player.events({
+        'click': function () {
+            Session.set("selected_player", this._id);
+        }
+    });
+    
+
     var delete_cookie = function() {
         document.cookie =  'id=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+
+
+    var uuid, lat, lng;
+    
+    var updatePlayer = function(uuid, lat, lng) {
+        lat = randomize(lat);
+        lng = randomize(lng);
+
+        if (Players.find( { name: uuid } ).count() == 0) {
+            Players.insert(
+                { name: uuid,  lat: lat, lng: lng },
+                { name: uuid }
+                // upsert doesn't work yet in Minimongo
+            );
+        };
     }
 
     var markers = new OpenLayers.Layer.Markers( "Markers" );
@@ -46,62 +55,25 @@ if (Meteor.isClient) {
     var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection                                                      
     var zoom           = 12;
 
-    var uuid, lat, lng;
-    
-    var updatePlayer = function(uuid, lat, lng) {
-        if (Players.find( { name: uuid } ).count() == 0) {
-            Players.insert(
-                { name: uuid,  lat: lat, lng: lng },
-                { name: uuid },
-                true // upsert, but doesn't work
-            );
-        };
-    }
 
     var size = new OpenLayers.Size(21, 25);
     var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
     var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
     
 
+    var map, mapnik;
+
+    function GetLocation(location) {
+        lng = location.coords.longitude;
+        lat = location.coords.latitude;
+        var position = new OpenLayers.LonLat(lng, lat).transform(fromProjection, toProjection);
+        map.setCenter(position, zoom);
+        
+        map.addLayer(markers);
+    };
+
     Meteor.startup(function () {
-
-
-        var map = new OpenLayers.Map('map');
-        var mapnik         = new OpenLayers.Layer.OSM();
-        map.addLayer(mapnik);
-        
-
-        var random = function() {
-            var r = Math.random() - 0.5;
-            if (r < 0) {
-                r -= 0.5;
-            } else {
-                r += 0.5;
-            }
-            return r;
-        }
-        
-        var randomize = function(x) {
-            return x + random() * 0.01;
-        }
-        
-        function GetLocation(location) {
-            lng = location.coords.longitude;
-            lat = location.coords.latitude;
-            var position = new OpenLayers.LonLat(lng, lat).transform(fromProjection, toProjection);
-            map.setCenter(position, zoom);
-            
-            lat = randomize(lat);
-            lng = randomize(lng);
-            markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(lng, lat).transform(fromProjection, toProjection), icon));
-            map.addLayer(markers);
-            
-        };
-        navigator.geolocation.getCurrentPosition(GetLocation);
-    });
-
-    Meteor.autosubscribe(function() {
-        console.log('autosub');
+        console.log('startup');
 
         if (!document.cookie.match("uuid")) {
             uuid = somewhat_uuid();
@@ -110,7 +82,17 @@ if (Meteor.isClient) {
             uuid = document.cookie.replace('uuid=', '');
         }
 
-        updatePlayer(uuid, lat, lng);
+        map = new OpenLayers.Map('map');
+        mapnik = new OpenLayers.Layer.OSM();
+        map.addLayer(mapnik);
+        navigator.geolocation.getCurrentPosition(GetLocation);
+    });
+
+    Meteor.autosubscribe(function() {
+        console.log('autosub');
+
+        if (lat !== undefined)
+            updatePlayer(uuid, lat, lng);
         Players.find({}).map(function(val) {
             console.log(val.lat + ' ' + val.lng);
             markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(val.lng, val.lat).transform(fromProjection, toProjection), icon.clone()));
@@ -125,17 +107,5 @@ if (Meteor.isClient) {
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Players.insert({name: names[i], 
-                        score: Math.floor(Math.random()*10)*5
-                       });
-    }
   });
 }
