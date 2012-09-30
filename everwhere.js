68a02befba6f1db3f55e00a31606fec6043a890f
Kasper Souren <kasper@guaka.org>
2012-09-30 22:26:43 +0200

    everwhere.js

--- everwhere.js
+++ everwhere.js
@@ -1,18 +1,10 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players."
 
 
-
-
-
-
-// On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
     Players = new Meteor.Collection("players");
     Meteor.startup(function () {
 
         Meteor.publish("players", function () {
-            // Only 50 "Items" have enabled set to true
             return Players.find({});
         });
     });
