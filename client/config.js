a74ee0bbdc6408d83067ad93bb3866a5fde33b82
Kasper Souren <kasper@guaka.org>
2012-10-14 23:03:59 +0200

    fb permissions

---
+++ client/config.js
@@ -0,0 +1,5 @@
+
+Accounts.ui.config({requestPermissions: {facebook: ['email','publish_stream','user_about_me','user_hometown','user_checkins','friends_hometown','friends_location','friends_status','friends_checkins']}})
+
+
+
