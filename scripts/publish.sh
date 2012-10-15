c40b82cf4e8b633119d3c359c5dfbebc728fb775
Kasper Souren <kasper@guaka.org>
2012-10-15 14:07:00 +0200

    addingdate

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,10 @@
+cd ~/everwhere
+
 git push
+
+echo '<template name="last-commit">' > lastcommitdate.html
+git log -1 --format="%ci" >> lastcommitdate.html
+echo '</template>' >> lastcommitdate.html
+
 meteor deploy everwhe.re
 meteor deploy --debug everwhere.meteor.com
