c5de8534160e6af78185a979f5be71ff4f3cbf50
Kasper Souren <kasper@guaka.org>
2012-10-17 11:59:35 +0200

    --debug for everwhe.re

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,5 +6,5 @@ echo '<template name="last-commit">' > lastcommitdate.html
 git log -1 --format="%ci" >> lastcommitdate.html
 echo '</template>' >> lastcommitdate.html
 
-meteor deploy everwhe.re
+meteor deploy --debug everwhe.re
 meteor deploy --debug everwhere.meteor.com
