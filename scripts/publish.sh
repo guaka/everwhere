84fcebbf234a7dce068b7542d1aacbe44ff9b5e6
Kasper Souren <kasper@guaka.org>
2013-01-27 15:15:10 +0100

    rm jasmine stuff on everwh.re, deploy everything on everwhere.meteor.com

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,4 +6,15 @@ echo '<template name="last-commit">' > lastcommitdate.html
 git log -1 --format="%ci" >> lastcommitdate.html
 echo '</template>' >> lastcommitdate.html
 
+meteor deploy everwhere.meteor.com
+
+cp -av . /tmp/everwherenotest
+pushd /tmp/everwherenotest
+
+rm -rf client/lib/jasmine*
+rm -rf client/lib/contrib/jasmine*
+find . -iname "*.spec.coffee" -exec rm '{}' ';'
 meteor deploy --debug everwh.re
+
+popd
+rm -rf /tmp/everwherenotest
